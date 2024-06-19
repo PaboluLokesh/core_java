47a118a1ff0d8c097648caa8fd703c641dc86672
PaboluLokesh <pabolulsnlokesh@gmail.com>
2024-06-19 17:01:12 +0530

    first commit

---
+++ Ex41.java
@@ -0,0 +1,17 @@
+class Ex1
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+
+        for(int x = 1; x<=10; x++) {
+            for(int i = 1; i<=10; i++) {
+                System.out.print(i + " ");
+            }
+
+            system.out.println();
+        }
+
+        System.out.println();
+    }
+}
